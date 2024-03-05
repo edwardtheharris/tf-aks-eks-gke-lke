046ed1062386ec6dd960ace5abe1fb4d36ab8841
Xander Harris <xandertheharris@gmail.com>
2024-03-04 16:36:15 -0800

    Add aks module
    See #1
    
    @edwardtheharris - xandertheharris@gmail.com
    
    Xander Harris
    
    Changelog: changed

---
+++ aks/main.tf
@@ -0,0 +1,59 @@
+provider "azurerm" {
+  features = {}
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "aks-rg"
+}
+
+variable "aks_cluster_name" {
+  type    = string
+  default = "my-aks-cluster"
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "node_count" {
+  type    = number
+  default = 1
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B2s"
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = var.aks_cluster_name
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  service_principal {
+    client_id     = ""
+    client_secret = ""
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
