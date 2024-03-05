6aedc6ac68bc27136d83f1569ca4b3d8457dfabe
Xander Harris <xandertheharris@gmail.com>
2024-03-04 18:18:20 -0800

    Add AKS module
    See #1
    
    @edwardtheharris - xandertheharris@gmail.com
    
    Xander Harris
    
    Changelog: changed

--- main.tf
+++ main.tf
@@ -19,6 +19,8 @@ terraform {
 module "aks" {
   source = "./aks"
 
+  sp_client_id        = var.sp_client_id
+  sp_client_secret    = var.sp_client_secret
   resource_group_name = "tf-aks-gh"
   aks_cluster_name    = "tf-aks"
   location            = "West US 3"
