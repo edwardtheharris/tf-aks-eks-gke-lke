d3f580fd5a01bdb02a1865a39ab2950e0ad2b034
Xander Harris <xandertheharris@gmail.com>
2024-03-26 13:40:50 -0700

    add eks module
    See #2
    
    @edwardtheharris - xandertheharris@gmail.com
    
    Xander Harris
    
    Changelog: changed

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
-    default = "us-west-2"
-    description = "The region in which the EKS cluster will be deployed."
-    type = string
+  default     = "us-west-2"
+  description = "The region in which the EKS cluster will be deployed."
+  type        = string
 }
 
 variable "AWS_ACCESS_KEY_ID" {
-    default = ""
-    description = "Access key ID"
-    type = string
+  default     = ""
+  description = "Access key ID"
+  type        = string
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-    default = ""
-    description = "AWS secret access key"
+  default     = ""
+  description = "AWS secret access key"
 }
