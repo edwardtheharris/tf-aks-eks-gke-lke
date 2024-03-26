93c58712dcdb0b347db993a05509e0eaf47502e8
Xander Harris <xandertheharris@gmail.com>
2024-03-26 13:32:16 -0700

    add eks module
    See #2
    
    @edwardtheharris - xandertheharris@gmail.com
    
    Xander Harris
    
    Changelog: changed

--- eks/main.tf
+++ eks/main.tf
@@ -20,12 +20,36 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Create a VPC
+resource "aws_vpc" "k8s" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "k8s01" {
+  vpc_id     = aws_vpc.k8s.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "k8s01"
+  }
+}
+
+resource "aws_subnet" "k8s02" {
+  vpc_id     = aws_vpc.k8s.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "k8s02"
+  }
+}
+
+
 resource "aws_eks_cluster" "k8s" {
   name     = "k8s"
   role_arn = aws_iam_role.k8s.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.k8s1.id, aws_subnet.k8s2.id]
+    subnet_ids = [aws_subnet.k8s01.id, aws_subnet.k8s02.id]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
--- eks/variables.tf
+++ eks/variables.tf
@@ -1,9 +1,16 @@
 variable "aws_region" {
-    default = "us-west-2a"
+    default = "us-west-2"
     description = "The region in which the EKS cluster will be deployed."
     type = string
 }
 
-variable "sp_client_secret" {
+variable "AWS_ACCESS_KEY_ID" {
+    default = ""
+    description = "Access key ID"
     type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    default = ""
+    description = "AWS secret access key"
 }
